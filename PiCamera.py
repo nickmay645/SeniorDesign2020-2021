e405b7fbbd3a2fd364bfcc26e788802e9eaad12d
Preston Smith <ptssmith2@gmail.com>
2020-09-02 00:04:40 -0400

    Face tracking?

--- PiCamera.py
+++ PiCamera.py
@@ -3,11 +3,16 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import sys
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
+cascPath = sys.argv[1]
+FaceCascade = c2v.CascadeClassifier(cascPath)
+
 # allow the camera to warmup
 time.sleep(0.1)
 # capture frames from the camera
@@ -16,7 +21,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# and occupied/unoccupied text
 	image = frame.array
 	# show the frame
-	cv2.imshow("Frame", image)
+    imgGray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    faces = FaceCascade.detectMultiScale(imgGray,1.1,10)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(imgGray,(x,y),(x+w,y+h),(255,0.255),2)
+
+
+	cv2.imshow("Frame", imgGray)
 	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
