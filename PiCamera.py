9e6f3cdb6959656c54698a629749b36e6f7eb33d
Preston Smith <ptssmith2@gmail.com>
2020-09-02 00:12:08 -0400

    tab issue

--- PiCamera.py
+++ PiCamera.py
@@ -19,18 +19,16 @@ time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
-	image = frame.array
-	# show the frame
+    image = frame.array
+    # show the frame
     imgGray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     faces = FaceCascade.detectMultiScale(imgGray,1.1,10)
     for (x,y,w,h) in faces:
         cv2.rectangle(imgGray,(x,y),(x+w,y+h),(255,0.255),2)
-
-
-	cv2.imshow("Frame", imgGray)
-	key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("Frame", imgGray)
+    key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+    rawCapture.truncate(0)
 	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    if key == ord("q"):
+	    break
