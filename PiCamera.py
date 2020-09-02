a96a20eca234c784f0eb862e11216fcc1ddde91c
Preston Smith <ptssmith2@gmail.com>
2020-09-02 14:34:17 -0400

    Changed Back resolution and fps

--- PiCamera.py
+++ PiCamera.py
@@ -7,9 +7,9 @@ import sys
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 60
-rawCapture = PiRGBArray(camera, size=(320, 240))
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 FaceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # allow the camera to warmup
