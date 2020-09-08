e69a2279dae13a34a98b6bd72c90f3b4bd8487f1
Preston Smith <ptssmith2@gmail.com>
2020-09-08 17:19:51 -0400

    switched pins

--- servotest.py
+++ servotest.py
@@ -7,7 +7,7 @@ servoPIN = 27
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 5) # GPIO 17 for PWM with 50Hz
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
