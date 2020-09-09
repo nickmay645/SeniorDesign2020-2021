d8cef6c561c468e396b800f1e92ead438459f90a
Preston Smith <ptssmith2@gmail.com>
2020-09-08 20:12:28 -0400

    made it too sensitive. Finding middle ground

--- servotracking.py
+++ servotracking.py
@@ -65,11 +65,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         
 
         
-        if y < 180:
+        if y < 150:
             if verticalvalue >= 100:
                 verticalvalue = verticalvalue - 100
             pi.set_servo_pulsewidth(17, verticalvalue) # position anti-clockwise
-        elif y >= 180:
+        elif y >= 250:
             if verticalvalue <= 2400:
                 verticalvalue = verticalvalue + 100
             pi.set_servo_pulsewidth(17, verticalvalue) # position anti-clockwise
