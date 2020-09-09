59cf057ed804cdb93923e1cf6b4a56d677afae8f
Preston Smith <ptssmith2@gmail.com>
2020-09-08 22:58:38 -0400

    Removed show and rectangle framing for boosted performance

--- servotracking.py
+++ servotracking.py
@@ -33,7 +33,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     imgGray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     faces = FaceCascade.detectMultiScale(imgGray,1.1,10)
     for (x,y,w,h) in faces:
-        cv2.rectangle(imgGray,(x,y),(x+w,y+h),(255,0.255),2)
+        #cv2.rectangle(imgGray,(x,y),(x+w,y+h),(255,0.255),2)
         #Based on the position of the bounding box, the camera will move to keep in centered
         #Horizontal detection
         if x < 220: #min value
@@ -54,7 +54,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                 verticalvalue = verticalvalue + 100
             pi.set_servo_pulsewidth(17, verticalvalue) 
     #displays the viewfinder
-    cv2.imshow("Frame", imgGray)
+    #cv2.imshow("Frame", imgGray)
     key = cv2.waitKey(1) & 0xFF
 	# clears the stream in preparation for the next frame
     rawCapture.truncate(0)
