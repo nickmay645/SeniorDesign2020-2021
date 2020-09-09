f57be2d74e089e83fb28b53e9c11d255765e250e
Preston Smith <ptssmith2@gmail.com>
2020-09-09 01:15:49 -0400

    fixed out of bounds issue

--- servotracking.py
+++ servotracking.py
@@ -45,7 +45,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             if horizontalvalue <= 2400: #values of servo must be kept between 500 - 2500
                 #horizontalvalue = horizontalvalue + 100
                 for horizontalvalue in range(100):
-                    horizontalvalue = horizontalvalue + 5
+                    horizontalvalue = horizontalvalue + 1
                     pi.set_servo_pulsewidth(27, horizontalvalue) #sends command to servo
             #pi.set_servo_pulsewidth(27, horizontalvalue) #sends command to servo
         elif x >= 420: #max value
