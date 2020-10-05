2e15d644eb68bf7506d61fff8ca8a8f56c4fcb2a
Preston Smith <ptssmith2@gmail.com>
2020-10-05 15:49:27 -0400

    reading MISO pin for data out

--- pinreader.py
+++ pinreader.py
@@ -1,10 +1,9 @@
 
 
-from time import sleep           # Allows us to call the sleep function to slow down our loop
 import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
  
 GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
-INPUT_PIN = 2           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
+INPUT_PIN = 9           #Video Over SPI Slave Data Out
 GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
 
 # Start a loop that never ends
