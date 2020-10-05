7b0a667e4b1a075f044a2d9edfda9c2437a567d5
Preston Smith <ptssmith2@gmail.com>
2020-10-05 15:14:27 -0400

    trying new approach for reading pins

--- pinreader.py
+++ pinreader.py
@@ -1,12 +1,16 @@
-import time
-import os
-import RPi.GPIO as GPIO
-import eeml
-GPIO.setmode(GPIO.BCM)
-DEBUG = 1
-LOGGER = 1
 
-GPIO.setup(2, GPIO.IN)
-input = GPIO.input(0)
 
-print(input)
+from time import sleep           # Allows us to call the sleep function to slow down our loop
+import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
+ 
+GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
+INPUT_PIN = 2           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
+GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
+
+# Start a loop that never ends
+while True: 
+           if (GPIO.input(INPUT_PIN) == True): # Physically read the pin now
+                    print('3.3')
+           else:
+                    print('0')
+           sleep(1);           # Sleep for a full second before restarting our loop
