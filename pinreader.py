c2ccfeb216defc2b73b0e12d0c97356a42e1be49
Preston Smith <ptssmith2@gmail.com>
2020-10-05 15:22:35 -0400

    trying a different GPIO standard

--- pinreader.py
+++ pinreader.py
@@ -3,8 +3,8 @@
 from time import sleep           # Allows us to call the sleep function to slow down our loop
 import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
  
-GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
-INPUT_PIN = 9           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
+GPIO.setmode(GPIO.BOARD)           # Set's GPIO pins to BCM GPIO numbering
+INPUT_PIN = 3           # Sets our input pin, in this example I'm connecting our button to pin 4. Pin 0 is the SDA pin so I avoid using it for sensors/buttons
 GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
 
 # Start a loop that never ends
