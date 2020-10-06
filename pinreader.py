e6f685d14d49e891cfed95ecb5e8bfc1fc8b2589
Preston Smith <ptssmith2@gmail.com>
2020-10-06 18:46:45 -0400

    created interface to switch between pins

--- pinreader.py
+++ pinreader.py
@@ -3,7 +3,8 @@
 import RPi.GPIO as GPIO           # Allows us to call our GPIO pins and names it just GPIO
  
 GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
-INPUT_PIN = 9           #Video Over SPI Slave Data Out
+INPUT_PIN = input("Enter pin to read: ")
+#INPUT_PIN = 3           #Video Over SPI Slave Data Out
 GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
 
 counter = 0
