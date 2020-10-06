285f17eabbe2016688dce722e21efdee36f04df7
Preston Smith <ptssmith2@gmail.com>
2020-10-06 18:29:33 -0400

    attempting to format 16 bit numbers

--- pinreader.py
+++ pinreader.py
@@ -6,6 +6,12 @@ GPIO.setmode(GPIO.BCM)           # Set's GPIO pins to BCM GPIO numbering
 INPUT_PIN = 9           #Video Over SPI Slave Data Out
 GPIO.setup(INPUT_PIN, GPIO.IN)           # Set our input pin to be an input
 
+counter = 0
+
 # Start a loop that never ends
 while True: 
-    print(GPIO.input(INPUT_PIN))
+    print(GPIO.input(INPUT_PIN),end='')
+    counter = counter + 1
+    if counter == 16:
+        counter = 0
+        print("/n")
