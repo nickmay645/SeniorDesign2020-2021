458314aa3b6a7b8b0e0fb9266b295b1d0778f55c
Preston Smith <ptssmith2@gmail.com>
2020-10-07 01:27:47 -0400

    forgot to add in setup command

--- wiringpy.py
+++ wiringpy.py
@@ -1,6 +1,7 @@
 
 import wiringpi
 
+wiringpi.wiringPiSetupGpio()  # For GPIO pin numbering
 wiringpi.pinMode(2, 1)       # Set pin 6 to 1 ( OUTPUT )
 wiringpi.digitalWrite(2, 1)  # Write 1 ( HIGH ) to pin 6
 wiringpi.digitalRead(2)      # Read pin 6
