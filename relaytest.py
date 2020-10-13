7fb2a85ea7670ff5d1e01d7bcf9a1fcf9dcac99f
Preston Smith <ptssmith2@gmail.com>
2020-10-13 18:20:03 -0400

    changed pin

--- relaytest.py
+++ relaytest.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
  
-RELAYGPIO = 25
+RELAYGPIO = 26
 GPIO.setup(RELAYGPIO, GPIO.OUT) # GPIO Assign mode
 GPIO.output(RELAYGPIO, GPIO.LOW) # out
 GPIO.output(RELAYGPIO, GPIO.HIGH) # on
