eda627669d5a5b6c0b8cac733f9077e8b8f2f298
Preston Smith <ptssmith2@gmail.com>
2020-10-15 20:53:10 -0400

    Deleted unneeded file

--- wiringpy.py
+++
@@ -1,7 +0,0 @@
-
-import wiringpi
-
-wiringpi.wiringPiSetupGpio()  # For GPIO pin numbering
-wiringpi.pinMode(2, 1)       # Set pin 2 to 1 ( OUTPUT )
-wiringpi.digitalWrite(2, 1)  # Write 1 ( HIGH ) to pin 2
-wiringpi.digitalRead(2)      # Read pin 2 
